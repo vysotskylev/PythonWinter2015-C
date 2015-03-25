59debd912e0ed0ddae2136ea38d240110cefd9ec
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2015-03-25 14:06:54 +0300

    More PyString

--- str.c
+++ str.c
@@ -1,10 +1,19 @@
 #include <Python.h>
 
 int main(int argc, char *argv[]) {
-  PyObject *res, *mul;
-  if(argc<2) exit(0);
+  PyObject *res;
+  int s, e, r;
+  if(argc<5) {
+    fprintf(stderr,"Usage: <string> <start> <end> <repeat>\n\n\
+Print string[start:end]*repeat");
+    exit(0);
+  }
+  s = atoi(argv[2]);
+  e = atoi(argv[3]);
+  r = atoi(argv[4]);
   res = PyString_FromString(argv[1]);
-  mul = PySequence_Repeat(res, 5);
-  printf("'%s'\n",PyString_AsString(mul));
+  res = PySequence_GetSlice(res, s, e);
+  res = PySequence_Repeat(res, r);
+  printf("'%s'\n",PyString_AsString(res));
   return 0;
 }
