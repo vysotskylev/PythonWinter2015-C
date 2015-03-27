9f3388c8eb9e67e2f948bb3f12a4fdba58d3f244
George V. Kouryachy (Fr. Br. George) <george@altlinux.ru>
2015-03-27 14:55:31 +0300

    Utilize flags
    - Introduce flags
    - Separate expand() method and memory reuse

--- testwoexp.py
+++ testwoexp.py
@@ -2,5 +2,14 @@ import woexp
 
 exp = woexp.WordExp("[p-t]*.*")
 print exp.result()
-exp = woexp.WordExp("*.NONEXIST")
+exp.expand("*.NONEXIST")
 print exp.result()
+exp.expand("$PWD")
+print exp.result()
+# TODO fix memoryleak here:
+for f in 0, woexp.WRDE_UNDEF:
+    try:
+        exp.expand("$NONEXIST", f)
+        print exp.result()
+    except MemoryError as ex:
+        print ex.args[0], exp.result()
